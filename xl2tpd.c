506bd8c0a6fb8576c333780ecc3a11038fc17db0
Paul Wouters <pwouters@redhat.com>
2012-12-11 01:16:31 -0500

    * last Russian space sneaked in

--- xl2tpd.c
+++ xl2tpd.c
@@ -811,7 +811,7 @@ void magic_lac_dial (void *data)
       if (lac->redial && (lac->rtimeout > 0))
       {
 #ifdef DEBUG_MAGIC
-        l2tp_log (LOG_DEBUG, "%s: Will redial in %d seconds\n", 
+        l2tp_log (LOG_DEBUG, "%s: Will redial in %d seconds\n",
              __FUNCTION__, lac->rtimeout);
 #endif
         tv.tv_sec = lac->rtimeout;
